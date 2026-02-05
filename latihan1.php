7f952d8c998bed8240cdd055d90041f125366b55
modul10 <alvithops@gmail.com>
2026-02-05 14:27:42 +0700

    Update konfigurasi profile dan tambah konten 1

--- latihan1.php
+++ latihan1.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-$angkaAcak = rand(1,100);
-echo "Angka acak : $angkaAcak <br>";
-$akar = sqrt(100);
-echo "Akar 100 : $akar <br>";
-$desimal = 123.6783;
-echo "Nilai desimal : $desimal <br>";
-$pembulatan = floor($desimal);
-echo "Pembulatan ke bawah : $pembulatan <br>";
-$pembulatanNaik = ceil($desimal);
-echo "Pembulatan ke atas : $pembulatanNaik <br>";
-$pendekatan = round($desimal, 2);
-echo "Pendekatan nilai : $pendekatan <br>";
-3
-?>
+    <?php
+    $angkaAcak = rand(1, 100);
+    echo "Angka acak : $angkaAcak <br>";
+    $akar = sqrt(100);
+    echo "Akar 100 : $akar <br>";
+    $desimal = 123.6783;
+    echo "Nilai desimal : $desimal <br>";
+    $pembulatan = floor($desimal);
+    echo "Pembulatan ke bawah : $pembulatan <br>";
+    $pembulatanNaik = ceil($desimal);
+    echo "Pembulatan ke atas : $pembulatanNaik <br>";
+    $pendekatan = round($desimal, 2);
+    echo "Pendekatan nilai : $pendekatan <br>";
+    3
+        ?>
 </body>
+
 </html>
