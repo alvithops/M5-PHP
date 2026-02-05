0672d2b9670ad8d541eba4c74da9465738e43744
modul10 <alvithops@gmail.com>
2026-02-05 14:39:16 +0700

    Update konfigurasi profile dan tambah konten 21

--- setanggal.php
+++ setanggal.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-$angka_hari = $_POST["angka_hari"];
-$bulan = $_POST["bulan"];
-$tahun = $_POST["tahun"];
-$time_tanggal = mktime(0,0,0,$bulan,$angka_hari,$tahun);
-$tanggal = date("l, j F Y", $time_tanggal);
-echo "Format Tanggal: $tanggal";
-?>
+    <?php
+    $angka_hari = $_POST["angka_hari"];
+    $bulan = $_POST["bulan"];
+    $tahun = $_POST["tahun"];
+    $time_tanggal = mktime(0, 0, 0, $bulan, $angka_hari, $tahun);
+    $tanggal = date("l, j F Y", $time_tanggal);
+    echo "Format Tanggal: $tanggal";
+    ?>
 </body>
+
 </html>
