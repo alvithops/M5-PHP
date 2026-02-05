3a5bfa21b618f11dbfdaba9206099777024e2cd1
modul10 <alvithops@gmail.com>
2026-02-05 14:30:58 +0700

    Update konfigurasi profile dan tambah konten 6

--- latihan2.php
+++ latihan2.php
@@ -1,22 +1,24 @@
 <!DOCTYPE html>
 <html>
+
 <body>
-<?php
-$str = "nama saya Susi Susanti";
-$str2=strtolower($str);
-echo $str2.'<br>';
-$str2=strtoupper($str);
-echo $str2.'<br>';
-$str2=ucfirst($str);
-echo $str2.'<br>';
-$str2=ucwords($str);
-echo $str2.'<br>';
-$str2=strrev($str);
-echo $str2.'<br>';
-$panjang=strlen($str);
-echo 'Panjang string: '.$panjang.'<br>';
-$str2 = substr($str, 0, 4);
-echo $str2.'<br>';
-?>
+    <?php
+    $str = "nama saya Susi Susanti";
+    $str2 = strtolower($str);
+    echo $str2 . '<br>';
+    $str2 = strtoupper($str);
+    echo $str2 . '<br>';
+    $str2 = ucfirst($str);
+    echo $str2 . '<br>';
+    $str2 = ucwords($str);
+    echo $str2 . '<br>';
+    $str2 = strrev($str);
+    echo $str2 . '<br>';
+    $panjang = strlen($str);
+    echo 'Panjang string: ' . $panjang . '<br>';
+    $str2 = substr($str, 0, 4);
+    echo $str2 . '<br>';
+    ?>
 </body>
+
 </html>
