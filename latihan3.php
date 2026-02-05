f60cded54e0e661950ad88151092f1d4a9ae68db
modul10 <alvithops@gmail.com>
2026-02-05 14:34:30 +0700

    Update konfigurasi profile dan tambah konten 13

--- latihan3.php
+++ latihan3.php
@@ -3,7 +3,7 @@
 
 <body>
     <?php
-    $nominal = 2500000.45678;
+    $nominal = 2500000.45678
     echo number_format($nominal) . "<br>";
     echo number_format($nominal, 2) . "<br>";
     echo number_format($nominal, 2, ",", ".");
