6a84e38f3460330afa174be5b1ce435107466a81
modul10 <alvithops@gmail.com>
2026-02-05 14:31:59 +0700

    Update konfigurasi profile dan tambah konten 8

--- latihan2.php
+++ latihan2.php
@@ -3,7 +3,7 @@
 
 <body>
     <?php
-    $str = "nama saya Alvitho";
+    $str = "nama saya Alvitho Polycharsius";
     $str2 = strtolower($str);
     echo $str2 . '<br>';
     $str2 = strtoupper($str);
